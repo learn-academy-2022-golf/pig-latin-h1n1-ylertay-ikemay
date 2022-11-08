486bd985703eb64d7182c2fff5c8cb5c381ff56f
MikeSC27 <mcruzen89@gmail.com>
2022-11-08 14:04:24 -0800

    story 1 complete

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import butcherPigImage from './assets/butcherPig.jpeg'
 const App = () => {
 
   // ACTION ITEM: to make the development process easier there are some preassigned words in the input field, when you are ready for your full user experience delete the test words passed to useState and pass an empty string
-  const [userInput, setUserInput] = useState("apple through queen squeal fry fluent")
+  const [userInput, setUserInput] = useState("")
   const [inputTranslated, setInputTranslated] = useState("")
 
   // ACTION ITEM: the "myPigLatinCodeHere" function is where you will put your logic to translate the sentence entered by the user into Pig Latin
@@ -32,11 +32,19 @@ const App = () => {
       console.log("vowelsArray:", vowelsArray)
 
       // ACTION ITEM: your Pig Latin logic goes here!
+      // if(eachWord[0].indexOf(vowelsArray) === -1)  
 
+
+   
+      if (eachWord[0] === vowelsArray[0]) {
+            return eachWord + "way"
+      }
+
+      
     
 
       // ACTION ITEM: this return will be the output of your Pig Latin'd code
-      return eachWord
+      return eachWord 
     })
 
     // NO MODIFICATION NEEDED: once the code has been modified it gets joined from an array back to a string
